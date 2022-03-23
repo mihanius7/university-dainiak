0549ba67d79b0b49998e53bac37bf12d8b32ae11
Mihas Dainiak <mihanius@gmail.com>
2022-03-23 10:56:13 +0300

    datastructures package added and filled

---
+++ src/com/belhard/university/datastructures/MyArray.java
@@ -0,0 +1,41 @@
+package com.belhard.university.datastructures;
+
+public class MyArray implements MyIterable {
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean add(Object obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(Object obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean contains(Object obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
