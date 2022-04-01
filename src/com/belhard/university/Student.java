3758b557eff55c22191ff48e09632774c71043bd
Mihas Dainiak <mihanius@gmail.com>
2022-04-01 11:44:03 +0300

    Year of study calculation fixed (+1 year)

--- src/com/belhard/university/Student.java
+++ src/com/belhard/university/Student.java
@@ -7,84 +7,83 @@ import java.util.Objects;
 import com.belhard.Person;
 
 public class Student extends Person {
-	private Faculty faculty;
-	private double averageMark;
-	private LocalDate studyStartDate;
+    private Faculty faculty;
+    private double averageMark;
+    private LocalDate studyStartDate;
 
-	public Student(String firstName, String lastName) {
-		super(firstName, lastName);
-	}
+    public Student(String firstName, String lastName) {
+        super(firstName, lastName);
+    }
 
-	public Student(String firstName, String lastName, int yearOfBirth, int monthOfBirth, int dayOfBirth) {
-		super(firstName, lastName, yearOfBirth, monthOfBirth, dayOfBirth);
-	}
+    public Student(String firstName, String lastName, int yearOfBirth, int monthOfBirth, int dayOfBirth) {
+        super(firstName, lastName, yearOfBirth, monthOfBirth, dayOfBirth);
+    }
 
-	public Faculty getFaculty() {
-		return faculty;
-	}
+    public Faculty getFaculty() {
+        return faculty;
+    }
 
-	public void setFaculty(Faculty faculty) {
-		this.faculty = faculty;
-	}
+    public void setFaculty(Faculty faculty) {
+        this.faculty = faculty;
+    }
 
-	public double getAverageMark() {
-		return averageMark;
-	}
+    public double getAverageMark() {
+        return averageMark;
+    }
 
-	public void setAverageMark(double averageMark) {
-		if (averageMark >= 0 && averageMark <= 10)
-			this.averageMark = averageMark;
-		else
-			System.out.println("Mark " + averageMark + " isn't valid. Current average mark " + this.averageMark);
-	}
+    public void setAverageMark(double averageMark) {
+        if (averageMark >= 0 && averageMark <= 10)
+            this.averageMark = averageMark;
+        else
+            System.out.println("Mark " + averageMark + " isn't valid. Current average mark " + this.averageMark);
+    }
 
-	public void setStudyStartDate(int year, int month, int day) {
-		this.studyStartDate = LocalDate.of(year, month, day);
-	}
+    public void setStudyStartDate(int year, int month, int day) {
+        this.studyStartDate = LocalDate.of(year, month, day);
+    }
 
-	public long defineYearOfStudy() {
-		if (studyStartDate != null)
-			return ChronoUnit.YEARS.between(studyStartDate, LocalDate.now());
-		else
-			return -1;
-	}
+    public long defineYearOfStudy() {
+        if (studyStartDate != null)
+            return ChronoUnit.YEARS.between(studyStartDate, LocalDate.now()) + 1;
+        else
+            return -1;
+    }
 
-	@Override
-	public String toString() {
-		String output = "Student ";
-		output = output.concat(super.toString());
-		output = output.concat("Average mark " + averageMark);
-		return output;
-	}
+    @Override
+    public String toString() {
+        String output = "Student ";
+        output = output.concat(super.toString());
+        output = output.concat("Average mark " + averageMark);
+        return output;
+    }
 
-	@Override
-	public String introduceYourself() {
-		String output = String.format(
-				"Hi! My name is %s. I am %d years old and I'm a student %nof %s faculty during %d years.%n", firstName,
-				defineAge(), faculty.toString().toLowerCase(), defineYearOfStudy());
-		return output;
-	}
+    @Override
+    public String introduceYourself() {
+        return String.format(
+                "Hi! My name is %s. I am %d years old and I'm a student %nof %s faculty on %d-th year of study.%n", firstName,
+                defineAge(), faculty.toString().toLowerCase(), defineYearOfStudy());
+    }
 
-	@Override
-	public int hashCode() {
-		int result = super.hashCode();
-		result = 31 * result + (int) averageMark * 100;
-		result = 31 * result + (studyStartDate == null ? 0 : studyStartDate.hashCode());
-		result = 31 * result + (faculty == null ? 0 : faculty.hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (int) averageMark * 100;
+        result = 31 * result + (studyStartDate == null ? 0 : studyStartDate.hashCode());
+        result = 31 * result + (faculty == null ? 0 : faculty.hashCode());
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Student other = (Student) obj;
-		return super.equals(obj) && Objects.equals(studyStartDate, other.studyStartDate) && Objects.equals(faculty, other.faculty)
-				&& averageMark == other.averageMark;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Student other = (Student) obj;
+        return super.equals(obj) && Objects.equals(studyStartDate, other.studyStartDate) && Objects.equals(faculty, other.faculty)
+                && averageMark == other.averageMark;
+    }
 
 }
